1a45fd6bd501d6a64358e8ca51911d8b4e502857
Luka Brinar <luka.brinar@dlabs.si>
2019-05-11 12:39:56 +0200

    Add available sessions

--- app/main/available-sessions-component/available-sessions.component.js
+++ app/main/available-sessions-component/available-sessions.component.js
@@ -12,16 +12,27 @@
         .module('MainModule')
         .component('availableSessionsComponent', availableSessionsComponent);
 
-    function availableSessionsController () {
+    function availableSessionsController (webSocketService, $scope) {
         var $ctrl = this;
 
         $ctrl.$onInit = $onInit;
 
+        $ctrl.arrayOfSessions = [];
         
         //////////////////////////////
         
         function $onInit () {
             
+            webSocketService.getSocket().addEventListener('message', function (event) {
+                var parsed = JSON.parse(event.data);   
+                if(parsed.command === 'create_session') {
+                    parsed.payload.stories.forEach(story => {
+                        $ctrl.arrayOfSessions.push(story);
+                    });
+                    $scope.$apply();
+                }
+            })
+
         }
 
     }
